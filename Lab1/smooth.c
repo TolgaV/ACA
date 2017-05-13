a042016c0be9cd06eab541ed322d497a279a895c
TOLGA <tolga.varol@outlook.com>
2017-05-14 00:32:26 +0200

    Loops unrolled and some optimization is made

--- Lab1/smooth.c
+++ Lab1/smooth.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/* Optimize the code, then conver it to MIPS64 Instruction Set */
+
 #define N_SAMPLES	5
 #define N_COEFFS	3
 
@@ -9,34 +9,35 @@ double	result[N_SAMPLES];
 
 void smooth(double sample[], double coeff[], double result[], int n)
 {
-	int i, j;
+	int i;
 	double norm=0.0;
 
-	for (i=0; i<N_COEFFS; i++)
-		norm+= coeff[i]>0 ? coeff[i] : -coeff[i];
+	norm+= coeff[0]>0 ? coeff[0] : -coeff[0];
+	norm+= coeff[1]>0 ? coeff[1] : -coeff[1];
+	norm+= coeff[2]>0 ? coeff[2] : -coeff[2];
 
-	for (i=0; i<n; i++) 
-	{
-		if (i==0 || i==n-1)
-		{
-			result[i] = sample[i];
-		}
-    		else 
-    		{
-			result[i]=0.0;
-			for (j=0; j<N_COEFFS; j++)
-				result[i] += sample[i-1+j]*coeff[j];
-			result[i]/=norm;
-		}
+	result[0] = sample[0];
+	for (i=2; i<n-1; i++){
+		result[i] = 0.0;
+		result[i] += sample[i-1]*coeff[0];				//some sort of windowing
+		result[i] += sample[i]*coeff[1];
+		result[i] += sample[i+1]*coeff[2];
+		result[i] /= norm;
 	}
+	result[1] = 0.0;									//unrolled for 1 less branch
+	result[1] += sample[0]*coeff[0];
+	result[1] += sample[1]*coeff[1];
+	result[1] += sample[2]*coeff[2];
+	result[1] /= norm;
+	result[n-1] = sample[n-1];
 }
 
-int main(int argc, char *arvg[]) 
+int main(int argc, char *arvg[])
 {
 	int i;
 
-	if (N_SAMPLES>=N_COEFFS)
-		smooth(sample, coeff, result, N_SAMPLES);
+	if (N_SAMPLES>=3)									//N_SAMPLES is always > 3
+		smooth(sample, coeff, result, N_SAMPLES);		//so n is always > 3
 
 	for (i=0; i<N_SAMPLES; i++)
 		printf("%f\n", result[i]);
