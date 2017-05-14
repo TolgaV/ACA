ac2028c95afa570cab21dbdcf8640a35c9900586
TolgaV <tolga.varol@outlook.com>
2017-05-15 01:43:06 +0200

    Works for both -ve & +ve coefficients up to norm

--- Lab1/smooth.s
+++ Lab1/smooth.s
@@ -11,51 +11,44 @@ result: 		.double 0.0, 0.0, 0.0, 0.0, 0.0
 
 				.text
 main:
-				lw		$t0, N_SAMPLES							; load content of RAM location into $t0: $t0 = N_SAMPLES
-				slti	$t1, $t0, #3							; set $t1 True, if $t0 < 3 (immediate value) : N_SAMPLES < 3 ?
-				;mtc1	$zero, $f0								; move data from integer reg to FP reg, we might need a 0 FP reg later
-				mtc1 	$zero, $f1								; $f1(norm) = 0.0				
-				li		$t3, #0									; NORMLOOP
-				li		$t4, #3									; NORMLOOP
-				and		$t5, $t5, $zero							; set an integer register to zero to use for aligning
-				l.d		$f2, coeff($zero)						; load double, $f2 = coeff[0]
-				beq		$t1, $zero, normer						; $t1 == 0 => !(N_SAMPLES > 2)
-				li      $v0, 10             					; terminate program run and
-				syscall                     					; Exit end or jump to print
-
+				lw		$t0, N_SAMPLES($zero)					; load content of RAM location into $t0: $t0 = N_SAMPLES			$t0 = R8
+				mtc1 	$zero, F1								; $f1(norm) = 0.0
+				mtc1	$zero, F10								; zero FP register
+				slti	$t1, $t0, 3								; set $t1 True, if $t0 < 3 (immediate value) : N_SAMPLES < 3 ?		$t1 = R9
+				;slti basically adds 1
+				daddi	$t3, $zero, 0							; NORMLOOP															$t3 = R11
+				daddi	$t4, $zero, 3							; NORMLOOP															$t4 = R12
+				and		$t5, $t5, $zero							; set an integer register to zero to use for aligning				$t5 = R13
+				l.d		F2, coeff($zero)						; load double, $f2 = coeff[0]
+				;beq		$t1, $zero, normer					; $t1 == 0 => !(N_SAMPLES > 2)
+				bne		$t1, $zero, exit						; in order not to have the branch stall we just continue with normer after decoding this
+				
 normer:			
-				c.lt.d	$f1, $f2								; set FP flag if norm < coeff[i]
+				;c.lt.d	F1, F2									; set FP flag if norm < coeff[i]
+				c.le.d	F10, F2									; set FP flag if 0 =< coeff[i]
 				beq		$t3, $t4, setresult						; a more elegant method is needed since we check condition 3 times..
-				bc1t	coeff1t									; bc1f imm: branch to address if FP flag is FALSE       
-				bc1f	coeff1f									; bc1t imm: branch to address if FP flag is TRUE 
+				bc1t	coeffT									; coeff[i] > 0; bc1f imm: branch to address if FP flag is TRUE       
+				bc1f	coeffF									; coeff[i] =<0; bc1t imm: branch to address if FP flag is FALSE 
 				
 				
 coeffF:															; coeff[0] =< 0, so : norm += -coeff[0] => norm = -coeff[0]
-				sub.d	$f1, $f1, $f2							; norm = norm - coeff[0]
-				l.d		$f2, coeff($t5)
-				daddi	$t5, $zero, #8							; next coefficient
-				daddi	$t3, #1									; NORMLOOP
+				sub.d	F1, F1, F2								; norm = norm - coeff[0]
+				l.d		F2, coeff($t5)
+				;daddi	$t5, $zero, 8							; next coefficient
+				daddi	$t5, $t5, 8							; next coefficient
+				daddi	$t3, $t3, 1								; NORMLOOP
 				j 		normer									; jump back to normer
+				nop
 				
 				
 coeffT:															; coeff[0] > 0, so : norm += coeff[0] => norm = coeff[0]
-				add.d	$f1, $f1, $f2							; norm = norm + coeff[0]
-				l.d		$f2, coeff($t5)
-				daddi	$t5, $zero, #8							; next coefficient
-				daddi	$t3, #1									; NORMLOOP
-				j		normer									
-				
-setresult:
-				li		$t3, #1									; for i = 1
-				l.d		$f3, sample($zero)						; unrolled parts of the loop
-				s.d		$f3, result($zero)						; result[0] = sample[0]
-				lw		$t6, N_SAMPLES($zero)					; t6 = n for loop
-				; in for loop sth like i = 16, (offset of 2) i = i + 8 may work
-				; increment the sample pointer in loop and branch exit when next incrementation = address of N_SAMPLES
-				bneq 	N_SAMPLES, $t3, forloop
-				
-				;??? l.d	$f5, sample+N_SAMPLE*8($zero)? for result[n-1] = sample[n-1]
-				
+				add.d	F1, F1, F2								; norm = norm + coeff[0]
+				l.d		F2, coeff($t5)
+				;daddi	$t5, $zero, 8							; next coefficient
+				daddi	$t5, $t5, 8							; next coefficient
+				daddi	$t3, $t3, 1								; NORMLOOP t3 = t3 + 1;
+				j		normer
+				nop
+setresult:		
 
-				
-
+exit:
